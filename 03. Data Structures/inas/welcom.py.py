76f24214916de3ced2c33291c4af52fdae1fe6fa
inas22310002 <22310002@scme.edu.ps>
2024-11-03 09:53:00 +0200

    Create welcom.py.py

---
+++ 03. Data Structures/inas/welcom.py.py
@@ -0,0 +1,19 @@
+#list =[2,3,4,1,32]
+#print (len(list))
+#max(list)
+#print (max(list))
+#min(list)
+#print (min(list))
+#sum(list)
+#print (sum(list))
+#import random
+#random.shuffle(list)# Shuffle the itemas in the list
+list1=[4,1,2,32,3]
+list1=[2,3]
+list2=[1,9]
+list3=list1+list2
+print(list3)
+list3 =3*list1
+print(list3)
+list4 =list3[2:4]
+print(list4)
